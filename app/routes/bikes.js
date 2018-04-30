67b7158949ed1a57a9f5661d13e30793ae3a657c
Rafael Ferraro <ferraromv@gmail.com>
2018-04-30 16:33:51 -0300

    Fixing some warnings

--- app/routes/bikes.js
+++ app/routes/bikes.js
@@ -1,10 +1,11 @@
-import Ember from 'ember';
 import Route from '@ember/routing/route';
+import { Promise } from 'rsvp';
+import { later } from '@ember/runloop';
 
 export default Route.extend({
   model() {
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      Ember.run.later((function(){
+    return new Promise(function(resolve) {
+      later((function(){
         resolve(['bike1', 'bikeDSA']);
       }), 3000);
     });
